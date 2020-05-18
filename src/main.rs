9c62ca3047eb94d08f8164d1c3046c94d8f418b7
ttlekich <ttlekich@gmail.com>
2020-05-17 21:27:52 -0500

    stopped at getting flags off of App

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,37 @@ fn main() {
         .version("0.0.1")
         .author("Travis Lekich <ttlekich@gmail.com")
         .about("Get your weather on the command line.")
-        .arg("-z, --zip=[ZIP] 'weather by zip code.'")
-        .arg("-c, --city=[CITY] 'weather by city.'")
+        .arg("-z, --zip=[ZIP] 'by zip code.'")
+        .arg("-c, --city=[CITY] 'by city.'")
+        .arg("-f, --forecast 'forecast.'")
+        .arg("-w, --weather 'current weather.'")
         .arg("-d... 'Turn debugging information on'")
         .get_matches();
 
 
     if let Some(z) = matches.value_of("zip") {
-        // let resp = api::fetch_weather_by_zip(z);
-        let resp = api::fetch_forecast_by_zip(z);
+        // TODO how to get simple flags off of App?
+        if let 1 = matches.occurrences_of("w") {
+            let resp = api::fetch_weather_by_zip(z);
+            println!("{:#?}", resp);
+        }
+        if let 1 = matches.occurrences_of("f") {
+            let resp = api::fetch_forecast_by_zip(z);
+            println!("{:#?}", resp);
+        }
+        let resp = api::fetch_weather_by_zip(z);
         println!("{:#?}", resp);
     }
 
     if let Some(c) = matches.value_of("city") {
-        let resp = api::fetch_weather_by_city(c);
-        println!("{:#?}", resp);
+        if let Some(_) = matches.value_of("weather") {
+            let resp = api::fetch_weather_by_city(c);
+            println!("{:#?}", resp);
+        }
+        if let Some(_) = matches.value_of("forecast") {
+            let resp = api::fetch_weather_by_city(c);
+            println!("{:#?}", resp);
+        }
     }
 
     // You can see how many times a particular flag or argument occurred
